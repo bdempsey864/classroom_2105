2a8dc9d990c6dd66c98d4f7e091119121e076666
Blake Dempsey <bdempsey864@gmail.com>
2021-05-12 16:06:52 -0600

    Initial commit

--- lib/classroom.rb
+++ lib/classroom.rb
@@ -1,2 +1,24 @@
 class Classroom
+  attr_reader :subject, :students
+  def initialize(subject, students)
+    @subject = subject
+    @students = students
+    @add_student = add_student
+  end
+
+  def subject
+    'History'
+  end
+
+  def capacity
+    4
+  end
+
+  def students
+    []
+  end
+
+  def add_student(students)
+    @students << ['Mike', 'Megan', 'Bob']
+  end
 end
